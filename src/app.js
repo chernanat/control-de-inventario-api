7ace31e2b21ccaa6d157b57c4992662f0f0c6cc4
chernanat <chernan1234@hotmail.com>
2023-10-04 13:25:28 -0500

    conexion con la base de datos - Postgres

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 const app = require("./index");
 const { PORT } = require("./config");
+const sequelize = require("./db");
+//corremos el proyecto y la conexion a la db
+const main = async (req,res) => {
+  try {
+    await sequelize.authenticate();
+    console.log('connection succesfully!');
+    app.listen(PORT, () => {
+      console.log(`Escuchando en el puerto ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-app.listen(PORT, () => {
-  console.log(`Escuchando en el puerto ${PORT}`);
-});
+main();
+
+
--- src/controller/base.controller.js
+++ src/controller/base.controller.js
@@ -1,11 +1,11 @@
-const axios = require('axios');
-const rickAndMortyApi = async(req,res) => {
-   const response = await axios.get('https://rickandmortyapi.com/api/character');
-   console.log(response.data.results);;
-   res.send(response.data.results)
+const axios = require("axios");
+
+const rickAndMortyApi = async (req, res) => {
+  const response = await axios.get("https://rickandmortyapi.com/api/character");
+  console.log(response.data.results);
+  res.send(response.data.results);
 };
 
-
 module.exports = {
-    rickAndMortyApi
-}
+  rickAndMortyApi,
+};
