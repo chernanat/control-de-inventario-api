50bfd56e800100530064ff88036d851a43d95950
chernanat <chernan1234@hotmail.com>
2023-10-08 13:05:34 -0500

    pgadmin agregado -interfaz con postgres

--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require("sequelize");
 const {DB_NAME, DB_PASSWORD, DB_HOST} = require('./config')
 //PARAMETROS DE CONFIGURACION DE BASE DE DATOS - (nombre base de datos, motor de base de datos, password, host)
-const sequelize = new Sequelize(DB_NAME, DB_HOST, DB_PASSWORD, { //para dockerizar es necesario cambiar los parametros y que coincidan con 
+const sequelize = new Sequelize('prueba', 'postgres', 'carlos123', { //para dockerizar es necesario cambiar los parametros y que coincidan con 
   //los del archivo docker-compose.yml
-  host: DB_HOST,
+  host: 'postgres',
   dialect: 'postgres',
 });
 
